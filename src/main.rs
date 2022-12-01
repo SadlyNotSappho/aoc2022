b5f7003415a1b7a716a4b561b9ee724b81ddf70f
SapphicSapphire <112831566+SapphicSapphire@users.noreply.github.com>
2022-12-01 09:18:29 -0800

    set up aoc2022

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::fs::read_to_string;
+
+use clap::{arg, command, Command};
+fn cli() -> Command {
+    command!()
+        .arg(arg!([path] "Filepath for today's input")
+            .required(true))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = cli().get_matches();
+
+    let path = match matches.get_one::<String>("path") {
+        Some(p) => p,
+        None => panic!("No path arg")
+    };
+
+    let read = read_to_string(path).unwrap();
+
+    aoc2022::day1::part1(read);
 }
