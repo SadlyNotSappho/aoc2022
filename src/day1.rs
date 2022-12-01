433168e2a7cc9ce7d821e7310c61117916f3ddd5
SapphicSapphire <112831566+SapphicSapphire@users.noreply.github.com>
2022-12-01 09:34:04 -0800

    day 1 part 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,25 @@
 pub fn part1(input: String) {
-    println!("hi this is part 1: {}", input);
+    let vec: Vec<Vec<u32>> = input
+        .trim() // nightmare
+        .split("\n\n") // vec of `"1\n1\n1"`
+        .map(|c|
+            c.split('\n') // vec of `"1"`
+                .map(|c| {
+                    c.parse() // parses each thing
+                        .unwrap() // unwraps the result
+                })
+                .collect()) // vec of `1`
+        .collect(); // vec of vecs of `1`
+
+    let mut addedvec = vec![];
+
+    for cl in vec {
+        let mut num: u32 = 0;
+        for c in cl {
+            num += c;
+        };
+        addedvec.push(num);
+    };
+
+    println!("{:?}", addedvec.iter().max().expect("vec was empty, how did you manage that one"));
 }
